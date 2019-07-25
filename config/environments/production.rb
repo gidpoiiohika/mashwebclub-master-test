c1110c75d57b473a50fba9c1f6dea4b812719b56
gidpoiiohika <adacer73044210@gmail.com>
2019-07-25 10:47:09 +0300

    raise_delivery_errors = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ config.action_mailer.smtp_settings = {
   config.action_mailer.default_url_options = { :host => "mashweb-club.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
 
 
